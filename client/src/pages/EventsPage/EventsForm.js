7410d2aea3b0cd44e39c4fa1314f94d46525ce0e
Anna Lednyova <ashtart@gmail.com>
2023-11-16 18:40:48 +0200

    add events page (empty)

---
+++ client/src/pages/EventsPage/EventsForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventsForm = () => {
+    return (
+        <div>
+            EVENTS FORM
+        </div>
+    );
+}
+
+export default EventsForm;
---
+++ client/src/pages/EventsPage/EventsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import EventsForm from './EventsForm';
+
+const EventsPage = () => {
+    return (
+        <div>
+            <Header />
+            EVENTS
+            <EventsForm />
+        </div>
+    );
+}
+
+export default EventsPage;
